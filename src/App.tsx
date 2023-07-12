81e273bfbfe85752bae0246adda851f565b35644
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2023-07-13 06:09:25 +0700

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import "./App.css";
+import Nav from "./Nav";
 
 function App() {
   const msg = "This is from a variable.";
   const num = 0;
+
   return (
-    <div>
-      <div>This is just like HTML.</div>
-      <div>{msg}</div>
-      <div>Calculation: {num + 1}</div>
-      <div>Call function: {sum(1, 2)}</div>
-      <button onClick={alert}>Alert</button>
-    </div>
+    <>
+      <Nav />
+      <div className="wrapper">
+        <div>{msg}</div>
+        <div>Calculation: {num + 1}</div>
+        <div>Call function: {sum(1, 2)}</div>
+        <div className="header">Test CSS</div>
+        <button onClick={handleClick}>Click</button>
+      </div>
+    </>
   );
 }
 
 export default App;
 
-function alert() {
-  console.log("Alert");
-}
-
 function sum(a: number, b: number) {
   return a + b;
+}
+
+function handleClick() {
+  alert("Hello!");
 }
---
+++ src/Nav.tsx
@@ -0,0 +1,11 @@
+function Nav() {
+  return (
+    <div className="nav">
+      <span>🔥</span>
+      <span>Home</span>
+      <span>About Me</span>
+    </div>
+  );
+}
+
+export default Nav;
